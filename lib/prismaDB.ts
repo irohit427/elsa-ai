91016642a50af5c7f14bfa85c0be5ad315024906
Rohit <irohit427@users.noreply.github.com>
2023-09-05 12:58:10 +0530

    Updated PrismaDB init

--- lib/prismaDB.ts
+++ lib/prismaDB.ts
@@ -1,10 +1,5 @@
 import { PrismaClient } from "@prisma/client"
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
-
-const prismadb = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
+const prismadb = new PrismaClient();
 
 export default prismadb;
